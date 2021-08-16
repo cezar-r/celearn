77611b8d1aa08967f8c88b173f1e0837b37a661a
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-08-16 13:01:46 -0700

    Update math_ops.py

--- celearn/math_ops.py
+++ celearn/math_ops.py
@@ -1,4 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Math operations that are commonly used"""
+
 def mode(arr):
+	"""Finds the most common element in a list
+	
+	Parameters
+	----------
+	arr: list
+	
+	Retunrs
+	-------
+	sorted(list(counts.items()), key = lambda x : x[1])[::-1][0][0]: Any type
+	"""
 	counts = {}
 	for i in arr:
 		if i in counts:
