e1521091c1664009a162482bb25fb8d2709a3927
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-08-16 12:46:47 -0700

    Update model_selection.py

--- celearn/model_selection.py
+++ celearn/model_selection.py
@@ -1,6 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Contains functions that can be used to manipulate data so that it can be used for fitting models
+"""
+
 import random
 
 def train_test_split(X, y, test_size = .25):
+	"""Splits X and y into training and testing data
+	
+	Parameters
+	----------
+	X: 2D list
+	y: 2D list
+	test_size: float
+	
+	Returns
+	-------
+	X_train: 2D list
+	X_test: 2D list
+	y_train: 2D list
+	y_test: 2D list
+	"""
 	xy = list(zip(X, y))
 	random.shuffle(xy)
 	X, y = zip(*xy)
